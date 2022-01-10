fa405262b832224803248ef97d45a04bc90f7be3
meanjula <anjula.ghale@gmail.com>
2022-01-10 15:13:35 +0200

    created login route with validation

--- backend/router/authentication.js
+++ backend/router/authentication.js
@@ -11,12 +11,58 @@ router.get("/", (req, res) => {
 require("../database/connection");
 const User = require("../model/userSchema");
 
-//storing data to databse
-router.post("/register", (req, res) => {
+//storing data to databse using promises
+// router.post("/register", (req, res) => {
+//   const { first, last, username, email, phone, role, password, repassword } =
+//     req.body;
+
+//   //validating the user input
+//   if (
+//     !first ||
+//     !last ||
+//     !username ||
+//     !email ||
+//     !phone ||
+//     !role ||
+//     !password ||
+//     !repassword
+//   ) {
+//     return res.status(422).json({ error: "please fill the field" });
+//   }
+
+//   //using promises if email exist in databse return err else save data in databse
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({ error: "email already exist" });
+//       }
+//       // create user collection
+//       const user = new User({
+//         first,
+//         last,
+//         username,
+//         email,
+//         phone,
+//         role,
+//         password,
+//         repassword,
+//       });
+
+//       user
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ message: "registered successfully" });
+//         })
+//         .catch((err) => res.status(500).json({ error: "failed to register" }));
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+//storing data to databse using async await
+
+router.post("/register", async (req, res) => {
   const { first, last, username, email, phone, role, password, repassword } =
     req.body;
-  //console.log(`${first} ${last}`);
-  //res.json({ message: req.body });
 
   //validating the user input
   if (
@@ -31,31 +77,29 @@ router.post("/register", (req, res) => {
   ) {
     return res.status(422).json({ error: "please fill the field" });
   }
-  //using promises if email exist in databse return err else save data in databse
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "email already exist" });
-      }
-      // create user collection
-      const user = new User({
-        first,
-        last,
-        username,
-        email,
-        phone,
-        role,
-        password,
-        repassword,
-      });
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "email already exist" });
+    }
+    // create user collection
+    const user = new User({
+      first,
+      last,
+      username,
+      email,
+      phone,
+      role,
+      password,
+      repassword,
+    });
+    await user.save();
+    res.status(201).json({ message: "registered successfully" });
+  } catch (err) {
+    console.log(err);
+  }
 
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "registered successfully" });
-        })
-        .catch((err) => res.status(500).json({ error: "failed to register" }));
-    })
-    .catch((err) => console.log(err));
+  //using promises if email exist in databse return err else save data in databse
 });
+
 module.exports = router;
